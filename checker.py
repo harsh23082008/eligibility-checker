8e98b9f983267cf03326f21c7a3223d3339f8145
harsh23082008 <harshsdv54@gmail.com>
2025-08-27 08:56:46 +0530

    first commit

---
+++ checker.py
@@ -0,0 +1,12 @@
+print("hey whats your good name ?")
+name = input()
+print("hii",name )
+print("lets check you are eligible for voter id and licence or not ")
+
+age=int(input("what is your age ??\n"))
+
+
+if age>=18:
+    print("you are eligible for voter id and licence ")
+else:
+    print("you are under age and not eligible  ")
