ab997dfbb71d1e584a16e017d8fd318276b46bf4
harsh23082008 <harshsdv54@gmail.com>
2025-08-27 09:17:18 +0530

    updated checker.py  with code changes

--- checker.py
+++ checker.py
@@ -8,5 +8,7 @@ age=int(input("what is your age ??\n"))
 
 if age>=18:
     print("you are eligible for voter id and licence ")
+elif age == 17:
+    print("soon you are going to get eligible ")
 else:
     print("you are under age and not eligible  ")
